9bf50f8ecbc2df02871e43560279ea6b7d6d2a1d
D3NCyan <edwarddoltons@gmail.com>
2022-01-10 11:27:40 +0800

    use express json

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 const app = express();
-const PORT = 7500;
+app.use(express.json());
 
+const PORT = 7500;
 app.listen(PORT || process.env.PORT, () =>
   console.log(`Server running on ${PORT}`)
 );
