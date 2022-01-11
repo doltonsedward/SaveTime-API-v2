fcbac0888960c18c606c2a0618997e2333f8bf96
D3NCyan <edwarddoltons@gmail.com>
2022-01-11 10:59:29 +0800

    connect mongodb with mongoose in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,2 +1,8 @@
 import mongoose from "mongoose";
 import { MONGOOSE_CONNECTION } from "./common";
+
+mongoose.connect(MONGOOSE_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
