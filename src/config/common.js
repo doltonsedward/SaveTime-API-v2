f7d62cd0adb2908da079650cf9c712dd3902be3a
D3NCyan <edwarddoltons@gmail.com>
2022-01-11 12:22:05 +0800

    change export method in common file

--- src/config/common.js
+++ src/config/common.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT;
-export const MONGOOSE_CONNECTION = process.env.MONGOOSE_CONNECTION;
+const PORT = process.env.PORT;
+const MONGOOSE_CONNECTION = process.env.MONGOOSE_CONNECTION;
+
+export { PORT, MONGOOSE_CONNECTION };
