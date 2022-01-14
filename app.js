abe542ea0fd4c2be6e846800aa55a336b1c3193e
D3NCyan <edwarddoltons@gmail.com>
2022-01-14 09:07:17 +0800

    testing db mongoodb

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
-// import { PORT } from "./src/config/common";
-// import db from "./src/config/db";
+import { PORT } from "./src/config/common";
+import db from "./src/config/db";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+db.then(() => {
+  console.log("connected to database");
+}).catch((error) => {
+  console.log("error happend when to reach mongodb collection", error);
+});
 
 export default app;
