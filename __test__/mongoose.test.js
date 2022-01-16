f80c2ac1c761d87ca517d21851efa00ab8753033
D3NCyan <edwarddoltons@gmail.com>
2022-01-16 20:47:52 +0800

    fix testing with jest in mongoose.jest.js

--- __test__/mongoose.test.js
+++ __test__/mongoose.test.js
@@ -1,5 +1,16 @@
-describe("It should be true", () => {
-  it("should test that true === true", () => {
-    expect(true).toBe(true);
+import supertest from "supertest";
+import app from "../app";
+
+const request = supertest;
+describe("GET /api/v1", () => {
+  it("should return status code === 200", async () => {
+    const res = await request(app).get("/api/v1/");
+
+    expect(res.statusCode).toBe(200);
+  });
+  it("should return response status success", async () => {
+    const res = await request(app).get("/api/v1/");
+
+    expect(res.statusCode).toBe(200);
   });
 });
