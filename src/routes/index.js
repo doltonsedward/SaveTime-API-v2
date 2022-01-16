7a2caf7abd8c94034ef223a1f7ae179618271d31
D3NCyan <edwarddoltons@gmail.com>
2022-01-16 21:17:03 +0800

    add endpoint /categories in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { getCategories } from "../controllers/categories";
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send({
     status: "success",
-    message: "Hello..",
+    message: "Welcome to SaveTime API v2",
   });
 });
+
+router.get("/categories", getCategories);
 
 export default router;
